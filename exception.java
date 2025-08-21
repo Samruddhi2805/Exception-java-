7ff95bf10a9656508b31a22c2138ff4427f400b3
Samruddhi Nevse <nevsesamruddhi@gmail.com>
2025-08-21 21:23:00 +0530

    Add files via upload

---
+++ exception.java
@@ -0,0 +1,13 @@
+import java.util.* ;
+//demo Exception
+class excepdemo  //super class
+{
+  public static void main(String args[])
+   {
+      //try
+       //{
+         int res=10/0;
+         System.out.println("Result=" + res);
+       
+     }
+}
